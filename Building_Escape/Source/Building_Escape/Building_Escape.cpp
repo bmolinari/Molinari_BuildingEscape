faba796caa9d7c73d4cbaa7c523e5a68ec38975e
bmolinari <b.molinari@outlook.com>
2017-02-06 15:00:17 -0500

    Initial Commit

---
+++ Building_Escape/Source/Building_Escape/Building_Escape.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Building_Escape.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Building_Escape, "Building_Escape" );
---
+++ Building_Escape/Source/Building_Escape/Building_EscapeGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "Building_EscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDING_ESCAPE_API ABuilding_EscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
